0cd915c1a7d2c3414b83c8585e33526d79050866
MCFELA123 <157656298+MCFELA123@users.noreply.github.com>
2024-01-31 16:59:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-const urlsToCache = ["/", "app.js", "styles.css", "logo.svg"];
+const urlsToCache = ["/", "index.html", "script.js", "app.js", "styles.css", "logo.svg"];
 self.addEventListener("install", (event) => {
    event.waitUntil((async () => {
       const cache = await caches.open("pwa-assets");
--- script.js
+++ script.js
@@ -8,3 +8,30 @@ if ("serviceWorker" in navigator) {
         console.log(err);
       });
    }
+
+   document.onclick = hideMenu;
+   document.oncontextmenu = rightClick;
+
+   function hideMenu() {
+    document.getElementById("contextmenu").className = 'none';
+ setTimeout(ini, 250)
+  }
+
+   function rightClick(e) {
+  var menu = document.getElementById("contextmenu");
+
+    if(document.oncontextmenu) {
+      menu.className = 'block';
+      e.preventDefault();
+menu.style.top = e.pageY + 'px';
+menu.style.left = e.pageX + 'px';
+
+    }
+   }
+
+
+   function ini() {
+    document.getElementById("contextmenu").innerHTML = 
+    '<li>Home</li><li>Search</li><li>Bookmarks</li><li>Profile</li><li>Settings</li>';
+    
+   }
